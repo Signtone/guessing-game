5b1d1dd9e9c551f1d717d98fc29a1fdfe9407866
Signtone <actionx@list.ru>
2021-07-06 16:57:13 +0300

    feat: add implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
     constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
-
-module.exports = GuessingGame;
+ 
+     setRange(min, max) {
+         this._min = min;
+         this._max = max;
+     }
+ 
+     guess() {
+         return Math.round((this._min + this._max) / 2);
+     }
+ 
+     lower() {
+         this._max = this.guess();
+     }
+ 
+     greater() {
+         this._min = this.guess();
+     }
+ }
+ 
+ module.exports = GuessingGame;
